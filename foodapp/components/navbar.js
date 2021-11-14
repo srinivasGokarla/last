31fdf11fbc9d57cafcc818558dfd30638710c18d
srinivas217 <gokarla_fw11_270@masai.school>
2021-11-14 17:26:43 +0530

    foodapp

---
+++ foodapp/components/navbar.js
@@ -0,0 +1,16 @@
+function navbar(){
+    return ` 
+
+
+    <div id="options">
+    <img src="https://www.themealdb.com/images/logo-small.png"/>
+    <h3><a href="index.html">Home</a></h3>
+    <h3><a href="recipe.html">Recipe</a></h3>
+    <h3><a href="latest.html">Latest</a></h3>
+   
+    <input id="name" type="text" placeholder="search meals">
+ 
+ </div>`
+}
+
+export default navbar
